a9c9e473202ff5862701354201fd1aa853232330
David Knetemann <david.knetemann@studiohyperdrive.be>
2021-10-16 22:49:37 +0200

    changed mint button text and added favicon

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -6,7 +6,7 @@ function Hero() {
         <div className="hero">
             <h1>Welcome to Generative Pain</h1>
             <h4>10,000 unique and tweaked out Pain Fists, generated from tons of individualized traits, rooting from modern pop culture and current trends!</h4>
-            <button>LEARN MORE</button>
+            <button>MINT NOW</button>
         </div>
         <div className="hero-img-wrapper">
           <img src="/hero.jpeg" alt=""></img>
