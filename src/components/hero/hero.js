b47e964fe73567231bad5db043b7740d5011e441
David Knetemann <david.knetemann@studiohyperdrive.be>
2021-10-21 23:18:23 +0200

    review changes

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -4,7 +4,7 @@ function Hero() {
     return (
       <div id="home" className="hero-wrapper">
         <div className="hero">
-            <h1>Welcome to Generative Pain</h1>
+            <h1>Welcome to<br></br><span id="genpain">Generative Pain</span></h1>
             <h4>6942 unique and tweaked out Pain Fists, generated from tons of individualized traits, inspired by modern pop culture and current trends!</h4>
             <button>MINT NOW</button>
         </div>
--- src/components/rarity/rarity.js
+++ src/components/rarity/rarity.js
@@ -9,7 +9,7 @@ function Rarity() {
           <RarityItem img="/TEASER 1.jpg" title="HAND DRAWN" text="All pain nfts are hand drawn"/>
           <RarityItem img="/TEASER 2.jpg" title="ONE OF ONE" text="Find the rare 1/1 unique fists"/>
           <RarityItem img="/TEASER 3.jpg" title="TRAITS" text="Multiple backgrounds, fists, wrists and objects"/>
-          <RarityItem img="/TEASER 4.jpg" title="MINT PRICE" text="With a mint price of 0.03ETH"/>
+          <RarityItem img="/TEASER 4.jpg" title="MINT PRICE" text="With a painless mint price of 0.035ETH"/>
         </div>
       </div>
     );
