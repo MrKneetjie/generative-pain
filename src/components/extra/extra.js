2af87fad9e4159a3b9d05bbb4249eeb5c7886465
David Knetemann <david.knetemann@studiohyperdrive.be>
2021-11-04 18:57:01 +0100

    Final changes

--- src/components/extra/extra.js
+++ src/components/extra/extra.js
@@ -2,17 +2,24 @@ import './extra.css'
 
 function Extra() {
     return (
-      <div className="extra">
-        <div className="extra-item">
-          <img src="https://cdn.shopify.com/s/files/1/0595/2172/8678/files/Sucide_900x.jpg?v=1631099364"></img>
+      <div>
+        <div className="extra">
+          <div className="extra-item">
+            <img src="https://cdn.shopify.com/s/files/1/0595/2172/8678/files/Sucide_900x.jpg?v=1631099364"></img>
+          </div>
+          <div className="extra-item extra-item-content">
+            <hr></hr>
+            <h1>We do care</h1>
+            <h2>Silly goose, your mind is on the loose.</h2>
+            <p>Punching drywall or selling Generative Pain NFTs might help curve the urge to hurt yourself but actual professionals are here to help.</p>
+            <p>Let's Talk About it.</p>
+            <button>CALL A SUICIDE PROFESSIONAL</button>
+          </div>
         </div>
-        <div className="extra-item extra-item-content">
-          <hr></hr>
-          <h1>We do care</h1>
-          <h2>Silly goose, your mind is on the loose.</h2>
-          <p>Punching drywall or selling Generative Pain NFTs might help curve the urge to hurt yourself but actual professionals are here to help.</p>
-          <p>Let's Talk About it.</p>
-          <button>CALL A SUICIDE PROFESSIONAL</button>
+        <div className="help">
+          <p>National Suicide Prevention Lifeline</p>
+          <p>Hours: Available 24 Hours. </p>
+          <p>800-273-8255</p>
         </div>
       </div>
     );
